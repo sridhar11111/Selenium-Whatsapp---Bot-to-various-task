d6c7c77bf526ca3147cc3bcfadf6f46a9e16dda6
sridhar11111 <47394197+sridhar11111@users.noreply.github.com>
2020-06-06 20:15:48 +0530

    Update wat.py

--- wat.py
+++ wat.py
@@ -6,10 +6,10 @@ import time
 driver = webdriver.Chrome(executable_path="C:\seleniumdrivers\chromedriver.exe")
 driver.get("https://web.whatsapp.com/")
 time.sleep(10)
-msz = "Saurabh ka lund kala h "
-msz2 = "kala hee rhega"
+msz = "Your Message Here. "
+msz2 = "Your Second_Message Here."
 
-sr = driver.find_element_by_css_selector('span[title="saurabh"]')
+sr = driver.find_element_by_css_selector('span[title="saurabh"]')#here "Saurabh" is Name on whatsapp
 sr.click()
 i = 0
 time.sleep(5)
@@ -24,11 +24,3 @@ while True:
     ri.send_keys("count == " + str(i))
     driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]").click()
     i = i+1
-# while i < 10000:
-#     ri.send_keys(msz)
-#     press('enter')
-#     ri.send_keys(msz2)
-#     press('enter')
-#     ri.send_keys(msz3)
-#     press('enter')
-#     i = i+1
